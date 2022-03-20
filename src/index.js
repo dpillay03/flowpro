b1109a96ea3c6805409cb18f5ca9ed08e1bb4284
dpillay03 <dpillay03@gmail.com>
2022-03-19 18:22:56 -1000

    config complete

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Dashboard from "./components/Dashboard";
+import reportWebVitals from "./utils/reportWebVitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
